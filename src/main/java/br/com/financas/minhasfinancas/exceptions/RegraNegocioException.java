03a49745c452339c55ac301a706b35d7ed160484
Cavalcanti <ricardo.cavalcanti@accenture.com>
2023-10-15 09:27:09 -0300

    integration testing

---
+++ src/main/java/br/com/financas/minhasfinancas/exceptions/RegraNegocioException.java
@@ -0,0 +1,10 @@
+package br.com.financas.minhasfinancas.exceptions;
+
+public class RegraNegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = -4678588140877749183L;
+
+	public RegraNegocioException(String msg) {
+		super(msg);
+	}
+}
